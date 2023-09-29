430e3e432ee644b1b07a7f311aa184021799578c
lkedves <lkedves@localhost>
2023-09-29 19:11:20 +0200

    DustSimple - DustUtilsXml bugfix

--- DustSimple/src/hu/sze/milab/dust/utils/DustUtilsXml.java
+++ DustSimple/src/hu/sze/milab/dust/utils/DustUtilsXml.java
@@ -8,15 +8,12 @@ import org.w3c.dom.NodeList;
 public class DustUtilsXml implements DustUtilsConsts {
 
 	public static Element getFirstElement(Element e, String ns, String tagName) {
-		NodeList nl = e.getElementsByTagNameNS(ns, tagName);
+		NodeList nl = (null == ns) ? e.getElementsByTagName(tagName) : e.getElementsByTagNameNS(ns, tagName);
 		return (0 < nl.getLength()) ? (Element) nl.item(0) : null;
 	}
 
 	public static String getTagText(Element e, String ns, String tagName) {
 		Element ee = getFirstElement(e, ns, tagName);
-//		NodeList nl = e.getElementsByTagNameNS(ns, tagName);
-//		if ( 0 < nl.getLength() ) {
-//			String val = nl.item(0).getTextContent();
 		if ( null != ee ) {
 			String val = ee.getTextContent();
 			if ( !DustUtils.isEmpty(val) ) {
